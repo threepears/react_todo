26890106fe71c71b83af98039c36fa1a6aa47146
Michael Conrad <michael@threepears.com>
2016-07-06 14:02:33 -0500

    Configuration options

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     ],
     output: {
         path: './public',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/public/'
     },
     resolve: {
         modulesDirectories: ['node_modules', 'app'],
