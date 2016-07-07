fa56ee9abfd8ba9ee86ad42e19a2b16ff22bb3d6
Michael Conrad <michael@threepears.com>
2016-07-07 01:00:26 -0500

    Add resolveLoader to config file

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -42,5 +42,11 @@ module.exports = {
       { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
         loader: 'file'}
     ]
-  }
+  },
+
+  resolveLoader: {
+      modulesDirectories: [
+          '/node_modules'
+      ]
+},
 }
