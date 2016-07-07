7f6a51a9210616f9a39795bd9042a1d267e7287e
Michael Conrad <michael@threepears.com>
2016-07-06 23:20:23 -0500

    Pare down to essential code for troubleshooting

--- server.js
+++ server.js
@@ -6,117 +6,12 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const mysql = require('mysql');
-
-const JAWSDB_HOST = process.env.JAWSDB_HOST || 3000;
-const JAWSDB_USER = process.env.JAWSDB_USER || '';
-const JAWSDB_PASS = process.env.JAWSDB_PASS || '';
-const JAWSDB_PORT = process.env.JAWSDB_PORT;
-const JAWSDB_URL = process.env.JAWSDB_URL;
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', function(_, res) { res.sendFile(path.join(__dirname, './public')) });
 
 
 app.get('/', function(req, res) {
   console.log("HELLOOOOOOO!!!!!!!");
   res.send("HELLLOOOOOOO!");
 })
-
-// Establish database connection
-const connection = mysql.createConnection({
-  host: JAWSDB_HOST,
-  user: JAWSDB_USER,
-  password: JAWSDB_PASS,
-  // socketPath: '/tmp/mysql.sock',
-  database: 'tasks'
-});
-
-
-// Get list of tasks from database
-app.get('/tasks', function(req, res) {
-
-  connection.query('SELECT * FROM `tasklist`', function (error, results, fields) {
-    if (error) {
-      console.error('Error: ' + err.stack);
-      return;
-    }
-    res.send(results);
-
-  });
-});
-
-
-// Post new task to database
-app.post('/tasks/:task', function(req, res) {
-
-  const newTask = req.params.task;
-  const sql = 'INSERT INTO `tasklist` (taskname, iscompleted) VALUES (' + connection.escape(newTask) + ', "false")';
-
-  connection.query(sql, function (error, results, fields) {
-    if (error) {
-      console.error('Error: ' + error.stack);
-      return;
-    }
-    console.log(results);
-    res.send(results);
-  });
-});
-
-
-// Change completed status of task in database
-app.put('/tasks/complete/:task/:state', function(req, res) {
-
-  const newTask = req.params.task;
-  const newState = req.params.state;
-  const revisedState = (newState === "true") ? "false" : "true";
-  const sql = 'UPDATE `tasklist` SET iscompleted=' + connection.escape(revisedState) + ' WHERE taskname=' + connection.escape(newTask);
-
-  connection.query(sql, function (error, results, fields) {
-    if (error) {
-      console.error('Error: ' + error.stack);
-      return;
-    }
-    console.log(results);
-    res.send(results);
-  });
-});
-
-
-// Replace old task name with newly edited name
-app.put('/tasks/edit/:oldtask/:newtask', function(req, res) {
-
-  const oldTask = req.params.oldtask;
-  const newTask = req.params.newtask;
-  const sql = 'UPDATE `tasklist` SET taskname=' + connection.escape(newTask) + ' WHERE taskname=' + connection.escape(oldTask);
-
-  connection.query(sql, function (error, results, fields) {
-    if (error) {
-      console.error('Error: ' + error.stack);
-      return;
-    }
-    console.log(results);
-    res.send(results);
-  });
-});
-
-
-// Delete task from database
-app.delete('/tasks/:task', function(req, res) {
-
-  const newTask = req.params.task;
-  const sql = 'DELETE FROM `tasklist` WHERE taskname=' + connection.escape(newTask);
-
-  connection.query(sql, function (error, results, fields) {
-    if (error) {
-      console.error('Error: ' + error.stack);
-      return;
-    }
-    console.log(results);
-    res.send(results);
-  });
-});
 
 
 app.listen(PORT, () => {
