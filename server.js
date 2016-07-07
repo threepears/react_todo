12c0fcc9630fe7d7d5d75d0ae2adbe6906d29381
Michael Conrad <michael@threepears.com>
2016-07-07 18:13:21 -0500

    Implement new database connection and test, remove test text from index.html

--- server.js
+++ server.js
@@ -10,7 +10,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const connection = mysql.createConnection('mysql://tp4kkvyi5zttxvwm:skf9vi28nm5p91po@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/emspx5n4ycnze24b');
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
 
 app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
