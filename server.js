473d0ec6ef8a071612e44058af837b7b260e8595
Michael Conrad <michael@threepears.com>
2016-07-07 20:00:05 -0500

    Add app.get database command back to server.js again

--- server.js
+++ server.js
@@ -12,27 +12,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const connection = mysql.createConnection('mysql://tp4kkvyi5zttxvwm:skf9vi28nm5p91po@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/emspx5n4ycnze24b');
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
 
-  console.log('connected as sql id ' + connection.threadId);
-});
+//   console.log('connected as sql id ' + connection.threadId);
+// });
 
 // Get list of tasks from database
-// app.get('/tasks', function(req, res) {
+app.get('/tasks', function(req, res) {
 
-//   connection.query('SELECT * FROM `tasklist`', function (error, results, fields) {
-//     if (error) {
-//       console.error('Error: ' + err.stack);
-//       return;
-//     }
-//     res.send(results);
+  connection.query('SELECT * FROM `tasklist`', function (error, results, fields) {
+    if (error) {
+      console.error('Error: ' + err.stack);
+      return;
+    }
+    res.send(results);
 
-//   });
-// });
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
