3ff47786e5e6b13aa0f42a8c5a7ffc360c15d02c
Michael Conrad <michael@threepears.com>
2022-11-27 15:03:13 -0600

    I have no idea if this is working ... nothing is changing

--- public/app/components/header.js
+++ public/app/components/header.js
@@ -6,7 +6,7 @@ export default class ToDoHeader extends React.Component {
     return(
         <div className='header'>
           <img src="https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAXKAAAAJDMyMzExMzgxLTVkYjgtNDEyZS1hZmI5LWQxYjFhYWE1OGU1Zg.png" alt="Work Institute Logo" />
-          <h2>Note Taker Appy</h2>
+          <h2>Note Taker App</h2>
         </div>
       )
   }
